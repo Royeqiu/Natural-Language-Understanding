9ee142aedacbb1a086293d171585d0745d06e639
roye <roye1202@hotmail.com.tw>
2017-04-05 12:47:14 +0800

    Init Natural Language Understanding environment

---
+++ Natural-Language-Understanding/src/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by roye on 2017/4/5.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("ya");
+    }
+}
