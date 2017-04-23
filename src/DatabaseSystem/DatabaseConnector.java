ba9f02cfabe78682fa61ccc08780b19a8048ab44
roye <roye1202@hotmail.com.tw>
2017-04-23 15:51:26 +0800

    update postgresql connect

--- src/DatabaseSystem/DatabaseConnector.java
+++ src/DatabaseSystem/DatabaseConnector.java
@@ -1,7 +1,29 @@
 package DatabaseSystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by roye on 2017/4/22.
  */
 public class DatabaseConnector {
+    static public void main(String args[])
+    {
+        DatabaseConnector a=new DatabaseConnector();
+    }
+    DatabaseConnector()
+    {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = null;
+           
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
